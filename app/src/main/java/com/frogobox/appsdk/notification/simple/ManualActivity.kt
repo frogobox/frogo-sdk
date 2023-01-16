ccf5c58d041fbe79b7df2c2b3d1d58bb06c5babf
amirisback <faisalamircs@gmail.com>
2023-01-16 16:51:53 +0700

    update: permission check

--- app/src/main/java/com/frogobox/appsdk/notification/simple/ManualActivity.kt
+++ app/src/main/java/com/frogobox/appsdk/notification/simple/ManualActivity.kt
@@ -1,9 +1,12 @@
 package com.frogobox.appsdk.notification.simple
 
+import android.Manifest
 import android.app.PendingIntent
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.RemoteViews
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.frogobox.appsdk.R
@@ -66,6 +69,20 @@ class ManualActivity : BaseActivity<ActivityManualBinding>() {
             .setCustomBigContentView(expandedView) //.setStyle(new NotificationCompat.DecoratedCustomViewStyle())
             .build()
 
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
         notificationManager.notify(NOTIFICATION_ID, notification)
     }
 
